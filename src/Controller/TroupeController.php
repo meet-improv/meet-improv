835f49c6e52ae143f8841b72ad3e4e31f61cefb7
Yann-Loup PHAN VAN SONG <yann-loup.phanvansong@amadeus.com>
2018-11-11 23:49:36 +0100

    troupe edit form...

--- src/Controller/TroupeController.php
+++ src/Controller/TroupeController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Troupe;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Security\Voter\ContributorVoter;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\TroupeFormType;
 
 class TroupeController extends AbstractController
 {
@@ -59,16 +61,38 @@ class TroupeController extends AbstractController
     /**
      * @Route("/troupe/{identifier}/edit", name="troupe_identifier_edit")
      */
-    public function troupeEditByShortName($identifier, EntityManagerInterface $em)
+    public function troupeEditByShortName($identifier, EntityManagerInterface $em, Request $request)
     {
         $repository = $em->getRepository(Troupe::class);
         
         $troupe = $repository->findOneBy(array("identifier" =>$identifier));
-        
+
         $this->denyAccessUnlessGranted(ContributorVoter::EDIT_CONTRIBUTOR, $troupe);
+        
+        
+        $troupeform = $this->createForm(TroupeFormType::class);
+        $troupeform->handleRequest($request);
+        
+        if ($troupeform->isSubmitted() && $troupeform->isValid()) {
+            $troupe->setName($troupeform->getData()->getName());
+            $troupe->setDescription($troupeform->getData()->getDescription());
+            $troupe->setShortName($troupeform->getData()->getShortName());
+            $troupe->setLocation($troupeform->getData()->getLocation());
+            $em->flush();
+            
+            //return $this->redirectToRoute('tasklist_id',array(
+           //     'id'=>$tasklist->getId(),
+            //    'slug'=>$tasklist->getSlug(),
+              //  'from' => "update"
+                
+           // ));
+        }else{
+            $troupeform->setData($troupe);
+        }
         
         return $this->render('troupe/troupe_edit.html.twig', [
             'troupe' =>$troupe,
+            'troupeform'=>$troupeform->createView()
         ]);
     }
 }
